84349ef749f86d8bd04cce485a9acb8837939f05
Kristina Peru <krissy.germanov@gmail.com>
2022-05-24 16:16:13 -0600

    Adds links and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Projects from './Projects'
 import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 
 function App() {
   return (
@@ -10,24 +11,46 @@ function App() {
       </Fade>
       <h2>About Me</h2>
       <p>My name is Kristina Peru. I am a software engineer based in Denver, Colorado.</p>
+      <h2>Skills</h2>
+        <div className="skill-cont">
+          <Fade top cascade>
+            <div className="skill">JavaScript</div>
+            <div className="skill">React</div>
+            <div className="skill">Ruby</div>
+            <div className="skill">Rails</div>
+            <div className="skill">HTML</div>
+            <div className="skill">CSS</div>
+            <div className="skill">SQLite</div>
+            <div className="skill">Active Record</div>
+          </Fade>
+        </div>
       <h2>Projects</h2>
       <Projects />
-      <h2>Skills</h2>
-        <div>JavaScript</div>
-        <div>React</div>
-        <div>Ruby</div>
-        <div>Rails</div>
-        <div>HTML</div>
-        <div>CSS</div>
-        <div>SQLite</div>
-        <div>Active Record</div>
       <h2>Contact Me</h2>
-        <div>krissy.germanov@gmail.com</div>
-        <div>Linked In</div>
-        <div>GitHub</div>
-        <div>Huntr</div>
-        <div>Resume</div>
-        <div>Blog</div>
+        <div className="contact-cont">
+          <div className="contact">krissy.germanov@gmail.com</div>
+          <div className="divider"/>
+          <div className="contact">
+            <a href="https://www.linkedin.com/in/kristina-peru/" target="_blank" rel="noopener noreferrer">Linked In</a>
+          </div>
+          <div className="divider"/>
+          <div className="contact">
+            <a href="https://github.com/krisperu" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+          <div className="divider"/>
+          <div className="contact">
+            <a href="https://huntr.co/p/kristina-peru" target="_blank" rel="noopener noreferrer">Huntr</a>
+          </div>
+          <div className="divider"/>
+          <div className="contact">
+            <a href="https://docs.google.com/document/d/e/2PACX-1vTyhjeUg9HsquV_xLDq2pomMyzIgTsdyRVb8wW7yaXYpyELZbaeMsl3sedu5dZ9vuxipq-j9iFAAsTm/pub" target="_blank" rel="noopener noreferrer">Resume</a>
+          </div>
+          <div className="divider"/>
+          <div className="contact">
+            <a href="https://dev.to/krisperu" target="_blank" rel="noopener noreferrer">Blog</a>
+          </div>
+        </div>
+        <br></br>
     </div>
   );
 }
--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 
 function Projects() {
   return (
-    <div className="project-container">
-        <div className="project-card">
+    <Fade left cascade>
+        <div className="project-container">
+            <div className="project-card">
             <div>Before I Die</div>
             <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421550/abnnn8x510qrhnuvgbwm.png" alt="Before I Die" />
+            </div>
+            <div className="project-card">
+                <div>Furbaby Sitters Club</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421593/llxx5ek29iozckxripmk.png" alt="Furbaby Sitters Club" />
+            </div>
+            <div className="project-card">
+                <div>Reader</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421727/rbvuzsawgvqxmkbrfvsu.png" alt="Reader" />
+            </div>
+            <div className="project-card">
+                <div>Project Hopper</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper" />
+            </div>
+            <div className="project-card">
+                <div>Watch Out</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper" />
+            </div>
         </div>
-        <div className="project-card">
-            <div>Furbaby Sitters Club</div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421593/llxx5ek29iozckxripmk.png" alt="Furbaby Sitters Club" />
-        </div>
-        <div className="project-card">
-            <div>Reader</div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421727/rbvuzsawgvqxmkbrfvsu.png" alt="Reader" />
-        </div>
-        <div className="project-card">
-            <div>Project Hopper</div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper" />
-        </div>
-        <div className="project-card">
-            <div>Watch Out</div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper" />
-        </div>
-    </div>
+    </Fade>
   )
 }
 
