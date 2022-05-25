25df4b694e4220a066159ec414039a1cb0de9a85
Kristina Peru <krissy.germanov@gmail.com>
2022-05-25 15:35:27 -0600

    Makes proj images clickable

--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -3,36 +3,36 @@ import React from 'react'
 function Projects() {
   return (
     <div className="project-container">
-        <div className="project-card">
-        <div>
-            <a href="https://beforeidieapp.herokuapp.com/" target="_blank" rel="noopener noreferrer">Before I Die</a>
-        </div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421550/abnnn8x510qrhnuvgbwm.png" alt="Before I Die" />
-        </div>
-        <div className="project-card">
-            <div>
-                <a href="https://furbaby-sitters-club.herokuapp.com/" target="_blank" rel="noopener noreferrer">Furbaby Sitters Club</a>
+        <a href="https://beforeidieapp.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <div className="project-card">
+                <div>Before I Die</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421550/abnnn8x510qrhnuvgbwm.png" alt="Before I Die"/>
             </div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421593/llxx5ek29iozckxripmk.png" alt="Furbaby Sitters Club" />
-        </div>
-        <div className="project-card">
-            <div>
-                <a href="https://github.com/krisperu/Reader" target="_blank" rel="noopener noreferrer">Reader</a>
+        </a>
+        <a href="https://furbaby-sitters-club.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <div className="project-card">
+                <div>Furbaby Sitters Club</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421593/llxx5ek29iozckxripmk.png" alt="Furbaby Sitters Club"/>
             </div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421727/rbvuzsawgvqxmkbrfvsu.png" alt="Reader" />
-        </div>
-        <div className="project-card">
-            <div>
-                <a href="https://github.com/Skywrithin/Phase-2-project" target="_blank" rel="noopener noreferrer">Project Hopper</a>
+        </a>
+        <a href="https://github.com/krisperu/Reader" target="_blank" rel="noopener noreferrer">
+            <div className="project-card">
+                <div>Reader</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421727/rbvuzsawgvqxmkbrfvsu.png" alt="Reader"/>
             </div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper" />
-        </div>
-        <div className="project-card">
-            <div>
-                <a href="https://github.com/jkap100/phase-1-project" target="_blank" rel="noopener noreferrer">Watch Out</a>
+        </a>
+        <a href="https://github.com/Skywrithin/Phase-2-project" target="_blank" rel="noopener noreferrer">
+            <div className="project-card">
+                <div>Project Hopper</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653421494/su4sh4wp1e3n9f7cqwag.png" alt="Project Hopper"/>
             </div>
-            <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653511748/mjmz4pvnddxhxsxh2oys.png" alt="Project Hopper" />
-        </div>
+        </a>
+        <a href="https://github.com/jkap100/phase-1-project" target="_blank" rel="noopener noreferrer">
+            <div className="project-card">
+                <div>Watch Out</div>
+                <img className="img-crd" src="https://res.cloudinary.com/dhqfwgeap/image/upload/v1653511748/mjmz4pvnddxhxsxh2oys.png" alt="Watch Out"/>
+            </div>
+        </a>
     </div>
   )
 }
