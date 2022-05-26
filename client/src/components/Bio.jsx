cfbcd91effcc87792d774ca010cac6e50013f2fa
Kristina Peru <krissy.germanov@gmail.com>
2022-05-26 15:12:03 -0600

    bio

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -5,7 +5,7 @@ function Bio() {
     <span className="bio">
       <div className="bio-desc">Hi, my name is Kristina. I am a Full Stack Software Engineer. A background in Science and Mechanical Engineering taught me logic, analytical thinking, and gave me problem-solving capabilities. With years of experience in the administrative & retail fields, I am excellent at organization, finding creative solutions to problems, and I understand the importance of communication amongst clients and teammates.</div>
       <br />
-      <div className="bio-desc">My interests include DevOps, web design, and technology. My passion for coding, learning, growth, and success encourages me to continuously expand my skill set through new technologies. I am looking forward learning about and creating meaningful applications!</div>
+      <div className="bio-desc">My interests include DevOps, web design, and technology. My passion for coding, learning, growth, and success encourages me to continuously expand my skill set through new technologies. I am looking forward to learning about and creating meaningful applications!</div>
     </span>
   )
 }
