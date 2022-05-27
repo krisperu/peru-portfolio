cd0fa8cefefde2fa90ad1feba3c82b22c3057b9a
Kristina Peru <krissy.germanov@gmail.com>
2022-05-26 23:00:00 -0600

    resume

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
             </div>
             <div className="divider"/>
             <div className="contact">
-                <a href="https://docs.google.com/document/d/e/2PACX-1vTyhjeUg9HsquV_xLDq2pomMyzIgTsdyRVb8wW7yaXYpyELZbaeMsl3sedu5dZ9vuxipq-j9iFAAsTm/pub" target="_blank" rel="noopener noreferrer">Resume</a>
+                <a href="https://docs.google.com/document/d/e/2PACX-1vRgaqk5AlFaM4GgRdk7VzVO4ncwed2NOYxHkHwmYuxDFqIf68Z9MCEOijjAIXmQEQ/pub" target="_blank" rel="noopener noreferrer">Resume</a>
             </div>
             <div className="divider"/>
             <div className="contact">
